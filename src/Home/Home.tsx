30b45f585dbe6f7a2953ecb9662b9b09292f89cd
Ragini Pandey <pandeyragini55@gmail.com>
2024-02-04 23:40:53 +0530

    added add-content button

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardContent, Typography, Grid } from '@mui/material';
+import { Card, CardContent, Typography, Grid, Button } from '@mui/material';
 import { ContentForm } from '../Content/Content.interfaces';
 import { getAllContent } from './Home.service';
+import { useNavigate } from 'react-router-dom';
 
 const cardStyle = {
   minWidth: 275,
@@ -27,8 +28,19 @@ const linkStyle = {
   },
 };
 
+const addButtonStyle = {
+  marginBottom: '20px',
+};
+
+const cardContentStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  flexGrow: 1,
+};
+
 function Home() {
   const [allContent, setAllContent] = useState<ContentForm[] | any>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchAllContent = async () => {
@@ -43,28 +55,42 @@ function Home() {
     fetchAllContent();
   }, []);
 
+  const handleAddContentClick = () => {
+    navigate('/add-content');
+  };
+
   return (
-    <Grid container spacing={2}>
-      {allContent.map((item: ContentForm, index: number) => (
-        <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
-          <Card sx={cardStyle}>
-            <CardContent>
-              <Typography variant='h5' component='div'>
-                {item.title}
-              </Typography>
-              <Typography variant='body2' color='text.secondary'>
-                {item.description}
-              </Typography>
-            </CardContent>
-            <div style={linkStyle}>
-              <a href={item.link} target='_blank' rel='noopener noreferrer'>
-                Open Link
-              </a>
-            </div>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
+    <div>
+      <Button
+        variant='contained'
+        color='primary'
+        style={addButtonStyle}
+        onClick={handleAddContentClick}
+      >
+        Add Content
+      </Button>
+      <Grid container spacing={2}>
+        {allContent.map((item: ContentForm, index: number) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+            <Card sx={cardStyle}>
+              <CardContent sx={cardContentStyle}>
+                <Typography variant='h5' component='div'>
+                  {item.title}
+                </Typography>
+                <Typography variant='body2' color='text.secondary'>
+                  {item.description}
+                </Typography>
+              </CardContent>
+              <div style={linkStyle}>
+                <a href={item.link} target='_blank' rel='noopener noreferrer'>
+                  Open Link
+                </a>
+              </div>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
   );
 }
 
