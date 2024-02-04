1f5548e21e412981bf0447f3436d311bc0d67fdf
Ragini Pandey <pandeyragini55@gmail.com>
2024-02-04 21:16:52 +0530

    Created add content form and added validations

--- src/Content/Content.interfaces.ts
+++ src/Content/Content.interfaces.ts
@@ -1,2 +1,8 @@
 export interface IContentProps{
 }
+
+export interface ContentForm {
+  title: string;
+  description: string;
+  link: string;
+}
--- src/Content/Content.tsx
+++ src/Content/Content.tsx
@@ -1,7 +1,160 @@
-function Content() {
-  return (
-    <div>Content</div>
-  )
-}
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import axios from 'axios';
 
-export default Content
+import { ContentForm } from './Content.interfaces';
+
+const Content: React.FC = () => {
+  const [content, setContent] = useState<ContentForm>({
+    title: '',
+    description: '',
+    link: '',
+  });
+
+  const [errors, setErrors] = useState<{
+    title: string | null;
+    description: string | null;
+    link: string | null;
+  }>({
+    title: null,
+    description: null,
+    link: null,
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setContent({
+      ...content,
+      [e.target.name]: e.target.value,
+    });
+
+    if (e.target.name === 'title') {
+      validateTitle(e.target.value);
+    } else if (e.target.name === 'description') {
+      validateDescription(e.target.value);
+    } else if (e.target.name === 'link') {
+      validateLink(e.target.value);
+    }
+  };
+
+  const validateTitle = (value: string) => {
+    let error = null;
+
+    if (!value.trim()) {
+      error = 'Title is required';
+    } else if (value.length > 100) {
+      error = 'Title must not exceed 100 characters';
+    }
+
+    setErrors({
+      ...errors,
+      title: error,
+    });
+  };
+
+  const validateDescription = (value: string) => {
+    let error = null;
+
+    setErrors({
+      ...errors,
+      description: error,
+    });
+  };
+
+  const validateLink = (value: string) => {
+    let error = null;
+
+    if (!value.trim()) {
+      error = 'Link is required';
+    } else {
+      const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+      if (!urlRegex.test(value)) {
+        error = 'Invalid URL';
+      }
+    }
+
+    setErrors({
+      ...errors,
+      link: error,
+    });
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    validateTitle(content.title);
+    validateDescription(content.description);
+    validateLink(content.link);
+
+    if (!errors.title && !errors.description && !errors.link) {
+      try {
+        const response = await axios.post('https://evallo-asssignment-api-url', content, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        console.log('API response:', response.data);
+
+      } catch (error) {
+        console.error('Error during API call:', error);
+      }
+    }
+  };
+
+  return (
+    <Container maxWidth='md'>
+      <Typography variant='h4' component='h1' align='center' gutterBottom>
+        Add Content
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label='Title'
+              fullWidth
+              margin='normal'
+              name='title'
+              value={content.title}
+              onChange={handleChange}
+              error={!!errors.title}
+              helperText={errors.title}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label='Description'
+              fullWidth
+              margin='normal'
+              name='description'
+              value={content.description}
+              onChange={handleChange}
+              error={!!errors.description}
+              helperText={errors.description}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label='Link'
+              fullWidth
+              margin='normal'
+              name='link'
+              value={content.link}
+              onChange={handleChange}
+              error={!!errors.link}
+              helperText={errors.link}
+            />
+          </Grid>
+        </Grid>
+        <Button type='submit' variant='contained' color='primary'>
+          Submit
+        </Button>
+      </form>
+    </Container>
+  );
+};
+
+export default Content;
