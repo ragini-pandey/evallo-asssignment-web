5d98556fa0a2328dcfd3965bedfa8ec7e18107fb
Ragini Pandey <pandeyragini55@gmail.com>
2024-02-05 08:02:08 +0530

    api integration

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const contentServiceInstance = axios.create({
-  baseURL: 'https://api.example.com',
+  baseURL: 'http://localhost:8000/api',
   headers: {
     'Content-Type': 'application/json'
   },
